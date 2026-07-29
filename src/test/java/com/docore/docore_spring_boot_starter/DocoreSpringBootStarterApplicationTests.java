25cfb377e79da27b3e60a1420563f8824f67bc9b
vDhinithya <dhinithya12@gmail.com>
2026-07-29 23:24:48 +0530

    chore: initialize project

---
+++ src/test/java/com/docore/docore_spring_boot_starter/DocoreSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docore.docore_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocoreSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
